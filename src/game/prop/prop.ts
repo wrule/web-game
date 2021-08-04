ed501a0f5c2e8ef1caee579c8fc0386b67192299
jimao <jimao@jimaodeMacBook-Pro.local>
2021-08-04 13:54:59 +0800

    修改注释

--- src/game/prop/prop.ts
+++ src/game/prop/prop.ts
@@ -2,7 +2,8 @@ import { Snapshot } from '../snapshot/snapshot';
 
 export interface IProp {
   /**
-   * 道具的快照列表，可以被摄像机采集且按顺序渲染
+   * 道具的快照列表
+   * 此快照列表可以被摄像机采集后按顺序渲染
    */
   Snapshots: Snapshot[];
 }

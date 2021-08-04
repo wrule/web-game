7becbf618beefbf0b2f6138c96c677b93519af95
jimao <jimao@jimaodeMacBook-Pro.local>
2021-08-04 23:45:23 +0800

    改造snapshot

--- src/game/snapshot/snapshot.ts
+++ src/game/snapshot/snapshot.ts
@@ -1,9 +1,28 @@
+import { IPoint } from '../geometry/point';
+import { Rect } from '../geometry/rect';
 import { Texture } from '../texture/texture';
 
 export class Snapshot {
   constructor(
-    public readonly x: number,
-    public readonly y: number,
-    public readonly texture: Texture
-  ) { }
+    point: IPoint,
+    public readonly texture: Texture,
+  ) {
+    this.point.x = point.x;
+    this.point.y = point.y;
+  }
+
+  public readonly point: IPoint = {
+    x: 0,
+    y: 0,
+  };
+
+  public get Scope() {
+    return new Rect(
+      this.point,
+      {
+        x: this.point.x + this.texture.dWidth - 1,
+        y: this.point.y + this.texture.dHeight - 1,
+      },
+    );
+  }
 }

06dd55e970b2fa6f8988a580cc8f7e2def341204
jimao <jimao@jimaodeMacBook-Pro.local>
2021-08-06 13:31:02 +0800

    point的Move

--- src/game/geometry/point.ts
+++ src/game/geometry/point.ts
@@ -1,7 +1,18 @@
+import { IOffset } from "./offset";
 
 export class Point {
   constructor(
     public x: number,
     public y: number,
   ) { }
+
+  public Move(offset?: IOffset) {
+    let x = this.x;
+    let y = this.y;
+    if (offset) {
+      x += offset.offsetX;
+      y += offset.offsetY;
+    }
+    return new Point(x, y);
+  }
 }
